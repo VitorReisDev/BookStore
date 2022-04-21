4781f6d050887af1ddd3a49b3bf2ce5b634639ec
VitorReisDev <hugov5323@gmail.com>
2022-04-21 15:01:00 -0300

    feat: create app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'carrinho',
+    component: CarrinhoComponent
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailsComponent
+  },
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
