9dbb9412737551e6af8fc62d73deaf456c3d1df4
VitorReisDev <hugov5323@gmail.com>
2022-04-21 15:00:46 -0300

    feat: add necessay imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarrinhoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
